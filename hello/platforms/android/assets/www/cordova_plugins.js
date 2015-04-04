63bdf3dcfefba21ed2beb9c56ec88b8e94fa3024
unknown <paulo.ferreira@NTB_DEVWEB_01.livraria.lc>
2015-04-04 18:12:49 -0300

    Primeiro exemplo de Cordova

---
+++ hello/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,25 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
+        "id": "org.apache.cordova.statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/com.phonegap.plugins.barcodescanner/www/barcodescanner.js",
+        "id": "com.phonegap.plugins.barcodescanner.BarcodeScanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.statusbar": "0.1.10",
+    "com.phonegap.plugins.barcodescanner": "2.0.1"
+}
+// BOTTOM OF METADATA
+});
